0e6af81cd0679b09c3b79c0ac5f06fba3b4b71c7
Grant Sander <gksander93@gmail.com>
2021-01-01 08:05:29 -0600

    Update Types.view.tsx

--- src/views/Types.view.tsx
+++ src/views/Types.view.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useQuery } from "react-query";
 import { QUERY_CACHE_KEYS } from "../consts";
 import { $api } from "../$api";
-import { Link } from "react-router-dom";
+import { PokeTypeChip } from "../components/PokeTypeChip";
 
 type TypesViewProps = {};
 
@@ -12,17 +12,22 @@ export const TypesView: React.FC<TypesViewProps> = () => {
     $api.fetchTypesList,
   );
 
-  if (status === "loading") {
-    return <p>Loading</p>;
-  }
-
   return (
-    <div>
-      {(data || []).map((type) => (
-        <div key={type.id}>
-          <Link to={`/types/${type.slug}`}>{type.slug}</Link>
+    <div className="container max-w-2xl py-6 px-2">
+      <div className="text-3xl font-bold">Types</div>
+      <div className="mb-4 text-gray-700">
+        Select a type to learn more about it.
+      </div>
+      {status === "loading" ? (
+        // S TODO: Better indicator
+        <span>Loading</span>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+          {(data || []).map((type) => (
+            <PokeTypeChip slug={type.slug} isBlock />
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 };
